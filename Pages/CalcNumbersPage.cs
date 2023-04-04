22384a5ae5039d6b3afd1dfb9985e0917b706bc6
Elena Uzunova <elena.ios.uzunova@gmail.com>
2023-04-04 14:03:25 +0300

    Add project files.

---
+++ Pages/CalcNumbersPage.cs
@@ -0,0 +1,62 @@
+﻿using OpenQA.Selenium;
+
+namespace CalculatorTestsPOM.Pages
+{
+    public class CalcNumbersPage
+    {
+        private WebDriver driver;
+        private const string baseUrl = "http://softuni-qa-loadbalancer-2137572849.eu-north-1.elb.amazonaws.com/number-calculator/";
+
+        //constructor
+        public CalcNumbersPage(WebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+
+        //properties
+        public IWebElement Field1 => driver.FindElement(By.Id("number1"));
+        //public IWebElement Filed1 { get { return driver.FindElement(By.Id("number1")); } }
+        public IWebElement OperationField => driver.FindElement(By.Id("operation"));
+        public IWebElement Field2 => driver.FindElement(By.Id("number2"));
+        public IWebElement CalcButton => driver.FindElement(By.Id("calcButton"));
+        public IWebElement ResultLabel => driver.FindElement(By.Id("result"));
+        public IWebElement ResetButton => driver.FindElement(By.Id("resetButton"));
+
+
+        //methods
+        public void Open()
+        {
+            driver.Navigate().GoToUrl(baseUrl);
+        }
+        public string GetPageTitle()
+        {
+            return driver.Title;
+        }
+        public bool IsPageOpen()
+        {
+            return driver.Url == baseUrl;
+        }
+
+        public string CalculateNumbers(string firtsValue, string operation, string secondValue)
+        {
+            Field1.SendKeys(firtsValue);
+            OperationField.SendKeys(operation);
+            Field2.SendKeys(secondValue);
+
+            CalcButton.Click();
+            return ResultLabel.Text;
+        }
+
+        public bool IsFormEmpty()
+        {
+            bool empty = (Field1.Text == "" && Field2.Text == "");
+            return empty;
+        }
+
+        public void ResetForm()
+        {
+            ResetButton.Click();
+        }
+    }
+}
---
+++ Tests/CalcNumbersPageTests.cs
@@ -0,0 +1,66 @@
+﻿using CalculatorTestsPOM.Pages;
+using NUnit.Framework;
+
+namespace CalculatorTestsPOM.Tests
+{
+    //inheritance
+    public class CalcNumbersPageTests : BaseTest
+    {
+        //private WebDriver driver;
+        private CalcNumbersPage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            //this.driver = new ChromeDriver();
+            //driver.Manage().Window.Maximize();
+            //driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            this.page = new CalcNumbersPage(driver);
+            page.Open();
+        }
+        //[TearDown]
+        //public void CloseBrowser()
+        //{
+        //    driver.Quit();
+        //}
+
+        [Test]
+        public void Test_CheckTitle()
+        {
+            Assert.That(page.GetPageTitle(), Is.EqualTo("Number Calculator"));
+        }
+        [Test]
+        public void Test_SumTwoPositiveNumbers()
+        {
+            var actualResult = page.CalculateNumbers("5", "+", "5");
+            Assert.That(actualResult, Is.EqualTo("Result: 10"));
+        }
+        [Test]
+        public void Test_MultiplyTwoPositiveNumbers()
+        {
+            var actualResult = page.CalculateNumbers("5", "*", "5");
+            Assert.That(actualResult, Is.EqualTo("Result: 25"));
+
+        }
+        [TestCase("5", "*", "5", "Result: 25")]
+        [TestCase("5", "+", "5", "Result: 10")]
+        [TestCase("5", "-", "5", "Result: 0")]
+        [TestCase("5", "/", "5", "Result: 1")]
+        [TestCase("5", "%", "aaa", "Result: invalid input")]
+        public void TestCalcTwoPositiveNumbers(
+                     string firstValue, string operation, string secondValue, string result)
+        {
+            var actualResult = page.CalculateNumbers(firstValue, operation, secondValue);//3 arguments
+            Assert.That(actualResult, Is.EqualTo(result));
+        }
+
+        [Test]
+        public void Test_CheckResetButton()
+        {
+            page.CalculateNumbers("5", "+", "8");
+            page.ResetForm();
+
+            Assert.True(page.IsFormEmpty());
+        }
+    }
+}
